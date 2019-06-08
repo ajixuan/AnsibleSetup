cbdfbf0068304be27f5d1fb56392919935b2f446
Allen Ji <aji@securitycompass.com>
2019-06-08 15:59:13 -0400

    Add install ansible in run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Determine os type
+os_family="$(grep -m1 NAME /etc/*release | cut -d\"=\" -f2)"
+
 #Make sure Ansible is installed
-if [ which ansible ]; then
-    echo "Install ansible"
-    exit 1
+if ! which ansible ; then
+	case $os_family in
+	"Arch*")
+		sudo pacman -Sy && yes | sudo pacman -S ansible ;;
+	*)
+	    echo "Please install ansible"
+	    exit 1
+		;;
+	esac
 fi
 
+# Determine available disks
+
+
 #Run playbook
-ansible-playbook
+ansible-playbook site.yml
